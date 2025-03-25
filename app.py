8c388df9f749a25f49e2a8424a692c02aea20bc2
tryggvi04 <tryggvi04@gmail.com>
2025-03-25 03:48:31 +0000

    this will work

--- app.py
+++ app.py
@@ -2,12 +2,6 @@ from flask import Flask, jsonify, request, render_template, send_from_directory
 from dotenv import load_dotenv
 import os
 
-try:
-    API_KEY = os.environ.get('sheets_api_key')
-except:
-    load_dotenv()
-    API_KEY = os.getenv('sheets_api_key')
-
 app = Flask(__name__)
 
 
@@ -39,17 +33,6 @@ def contact():
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-
-@app.route('/get_current_song_id', methods=['GET'])
-def get_current_song():
-
-    # 0 is the ID column in the google sheet
-    csv_song_link = ps.get_song_by_id(sheet_song[0], only_link=True)
-
-    # 4 is the link of the song, we're getting the id of the song, which is in the link
-    spotify_song_id = {"song_id":csv_song_link}
-
-    return wrap_up(spotify_song_id)
 
 if __name__ == '__main__':
     app.run()
